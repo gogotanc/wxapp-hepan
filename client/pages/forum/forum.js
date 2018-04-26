338b3513662284d3036b491276d2745e4acc5e2d
tanc <gogotanc@163.com>
2018-04-26 18:22:47 +0800

    更新了查看版块的功能

---
+++ client/pages/forum/forum.js
@@ -0,0 +1,64 @@
+// forum.js
+const util = require('../../utils/util')
+const app = getApp()
+
+Page({
+    data: {
+        loading: true,
+        list: []
+    },
+
+    // 进入某个版块
+    viewList: function (e) {
+        var board_id = e.currentTarget.dataset.id
+        wx.navigateTo({
+            url: '../list/list?id=' + board_id,
+        })
+    },
+
+    // 下拉刷新获取所有版块的数据
+    onPullDownRefresh: function() {
+        var that = this
+        wx.request({
+            // url: 'https://bbs.gogotanc.cn/detail',
+            url: 'https://bbs.gogotanc.cn/transfer',
+            method: 'POST',
+            data: {
+                r: 'forum/forumlist',
+                accessToken: app.globalData.loginFlag ? app.globalData.userInfo.token : '',
+                accessSecret: app.globalData.loginFlag ? app.globalData.userInfo.secret : '',
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            success: function(res) {
+
+                console.log(res.data)
+
+                // 请求出错的处理
+                if (res.data.rs == 0) {
+                    wx.stopPullDownRefresh() 
+                    wx.showToast({
+                        title: res.data.errcode,
+                        icon: 'none',
+                        duration: 2000,
+                        mask: true,
+                        success: () => {}
+                    });
+                    return
+                }
+
+                wx.stopPullDownRefresh()
+                that.setData({
+                    loading: false,
+                    list: res.data.list
+                })
+            }
+        })
+    },
+
+    // 生命周期函数
+    onLoad: function (options) {
+        wx.startPullDownRefresh()
+    }
+})
---
+++ client/pages/list/list.js
@@ -0,0 +1,93 @@
+// list.js
+const util = require('../../utils/util')
+const app = getApp()
+
+Page({
+    /**
+     * 某版块信息
+     * info {
+     *   title: '',
+     *   icon: '',
+     *   topicList: []
+     * }
+     */
+    data: {
+        loading: true,
+        board_id: null,
+        info: {}
+    },
+
+    // 处理请求返回的数据
+    dealData: function (res) {
+        var data = res.data
+        var info = {
+            title: data.forumInfo.title,
+            icon: data.forumInfo.icon,
+            topicList: data.list
+        }
+        return info
+    },
+
+    // 帖子详情
+    viewDetail: function (e) {
+        var topic_id = e.currentTarget.dataset.topicid
+        wx.navigateTo({
+            url: '../detail/detail?topicId=' + topic_id,
+        })
+    },
+
+    // 下拉刷新获取所有版块的数据
+    onPullDownRefresh: function() {
+        var that = this
+        wx.request({
+            // url: 'https://bbs.gogotanc.cn/detail',
+            url: 'https://bbs.gogotanc.cn/transfer',
+            method: 'POST',
+            data: {
+                r: 'forum/topiclist',
+                accessToken: app.globalData.loginFlag ? app.globalData.userInfo.token : '',
+                accessSecret: app.globalData.loginFlag ? app.globalData.userInfo.secret : '',
+                boardId: that.data.board_id
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            success: function(res) {
+
+                console.log(res.data)
+
+                // 请求出错的处理
+                if (res.data.rs == 0) {
+                    wx.stopPullDownRefresh() 
+                    wx.showToast({
+                        title: res.data.errcode,
+                        icon: 'none',
+                        duration: 2000,
+                        mask: true,
+                        success: () => {}
+                    });
+                    return
+                }
+
+                // 处理数据
+                var result = that.dealData(res)
+
+                console.log(result)
+
+                wx.stopPullDownRefresh()
+                that.setData({
+                    loading: false,
+                    info: result
+                })
+            }
+        })
+    },
+
+    // 生命周期函数
+    onLoad: function (options) {
+        this.setData({
+            board_id: options.id
+        })
+        wx.startPullDownRefresh()
+    }
+})
